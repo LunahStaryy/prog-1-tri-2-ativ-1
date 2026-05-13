2576847f71b0d35ff3c64041630699f6cfb23834
Your Name <you@example.com>
2026-05-13 15:06:21 -0300

    criamos o comeco bem simples com as funcoes mais basicas do prog-1-tri-2-ativ-1

---
+++ core.ts
@@ -0,0 +1,34 @@
+class Item {
+    constructor(public title: string) {
+    }
+}
+
+class TodoList {
+private items: Item[] = [];
+private filePath: string;
+
+constructor(filePath: string) {
+    this.filePath = filePath;
+}
+
+addItem(item: Item) {
+this.items.push(item);
+}
+
+removeItem(index: number) {
+    this.items.splice(index, 1);
+}
+
+getItems(): Item[]{
+    return [...this.items];
+}
+
+}
+
+
+//
+
+const todoList = new TodoList('todos.json')
+
+todoList.addItem(new Item("Wow"));
+console.log(todoList.getItems())

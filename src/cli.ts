d9046b4b8fe770c167378a49aa41509be0c91a9e
naty <advccosta10@gmail.com>
2026-05-27 15:06:56 -0300

    apenas falta o update

---
+++ src/cli.ts
@@ -0,0 +1,93 @@
+"strict mode";
+// CLI: Command Line Interface
+
+// import Xpto from './core'
+// import { Item as ItemApelido, TodoList as TodoListApelido } from './core'
+// import { Item, TodoList } from './core'
+
+import TodoListApelido, { Item } from './core'
+
+
+const todolist = new TodoListApelido('todolist.json')
+const params = process.argv
+const command = params[2]
+
+// ------------------------------------------------------------------------------
+// --- Comando List
+// ------------------------------------------------------------------------------
+
+if (command === 'list') {
+    const items = await todolist.getItems()
+    console.log("Lista de itens atuais!:")
+    items.forEach((item, index) => {
+        console.log(`${index}: ${item.title}`)
+    })
+    process.exit(0)
+}
+
+// ------------------------------------------------------------------------------
+// --- Comando Add
+// ------------------------------------------------------------------------------
+
+if (command === 'add') {
+    const value = params[3]
+
+    if (!value) {
+        console.error('Valor do item não pode ser nulo ou vazio')
+        process.exit(1)
+    }
+
+    try {
+        await todolist.addItem(new Item(value))
+    } catch (error) {
+        console.error('Erro ao adicionar item:', error)
+        process.exit(1)
+    }
+
+    console.log('Item adicionado com sucesso:', value)
+    process.exit(0)
+}
+ 
+// ------------------------------------------------------------------------------
+// --- Comando Remove
+// ------------------------------------------------------------------------------
+
+if (command == 'remove'){
+
+    const indexStr = params[3]
+    if(!indexStr) {
+        console.error('Índice do item a ser removido não pode ser nulo ou vazio')
+        process.exit(1)
+    }
+    const index = parseInt(indexStr)
+    if (isNaN(index)) {
+        console.error("índice precisa ser um número", indexStr)
+        process.exit(1)
+    }
+    await todolist.removeItem(index)
+    console.log('Item removido com sucesso:', index)
+    process.exit(0)
+}
+
+// ------------------------------------------------------------------------------
+// --- Comando Update
+// ------------------------------------------------------------------------------
+
+// deveria dizer qual posicao quer atualizar, e passar um novo parametro pra ele
+//ex: atualizar 0
+// ai digita o novo texto
+// ele aparece na lista com o novo texto
+
+
+// ------------------------------------------------------------------------------
+// --- Fallback para comandos não reconhecidos
+// ------------------------------------------------------------------------------
+
+if (command)
+    console.log(`Comando não reconhecido: ${command}`)
+
+console.log(`Comandos disponíveis:
+- add <item>: Adiciona um item à lista
+- remove <index>: Remove um item da lista por indice
+- list: Lista os itens atuais
+`)

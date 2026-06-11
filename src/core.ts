2b83a2653545385d7cce0b50a53094e929c169e8
Natyy <advccosta10@gmail.com>
2026-06-11 10:06:16 -0300

    Update core.ts

--- src/core.ts
+++ src/core.ts
@@ -1,90 +1,82 @@
-
-// Lydia Hallie : Event Loop
-
-/**
- * @todo
- * known issues:
- * - getItems needs to await loadListFromDisk()
- */
-
-// class Item_ { 
-//   public title: string
-//   constructor(title: string) {
-//     this.title = title
-//   }
-// }
+// lembrar o professor do to-do no começo do código
 
 class Item {
-  constructor(public title: string) { }
+    constructor(public title: string) { }
 }
 
-class TodoList {
-  private items: Promise<Item[]>
-  private filePath: string
+class TodoList { //updated
 
-  constructor(filePath: string) {
-    this.filePath = filePath
-    this.items = this.readListFromDisk()
+    private items: Promise<Item[]>
+    private filePath: string
+
+     constructor(filePath: string) {
+        this.filePath = filePath
+        this.items = this.readListFromDisk()
   }
 
-  private async saveListToDisk() {
-    const file = Bun.file(this.filePath)
-    const data = JSON.stringify(await this.items)
-    await file.write(data)
+    private async saveListToDisk() {
+        const file = Bun.file(this.filePath)
+        const data = JSON.stringify(await this.items)
+        await file.write(data)
   }
 
-  private async readListFromDisk() {
-    const file = Bun.file(this.filePath)
-    // const text = await file.text()
-    // const data = JSON.parse(text)
-    const data = await file.json()
-    const items: Item[] = data.map((v: any) => {
-      return new Item(v.title)
-    })
+    private async readListFromDisk() {
+        const file = Bun.file(this.filePath)
+        // const text = await file.text()
+        // const data = JSON.parse(text)
+        const data = await file.json()
+        const items: Item[] = data.map((v: any) => {
+            return new Item(v.title)
+        })
     return items
   }
-
-  /**
-   * Adiciona um novo item na lista de item
-   */
-  async addItem(item: Item) {
-    const items = await this.items
-    if (!item) 
-      throw 'item não pode ser nulo ou indefinido'
-    if (!item.title || !item.title.trim()) 
-      throw 'item.title não pode ser nulo ou indefinido'
-    items.push(item)
-    await this.saveListToDisk()
+  
+    /**
+     * Isso adiciona um novo item na lista de itens
+     */
+    async addItem(item: Item) {
+        const items = await this.items
+        if (!item) 
+        throw 'item não pode ser nulo ou indefinido'
+        if (!item.title || !item.title.trim()) 
+        throw 'item.title não pode ser nulo ou indefinido'
+        items.push(item)
+        await this.saveListToDisk()
   }
 
-  /**
-   * Remove um item da lista de item pelo indice
-   */
-  async removeItem(index: number) {
-    const items = await this.items
-    items.splice(index, 1)
-    await this.saveListToDisk()
+    /**
+     * Editar um item
+     */
+     async editItem(index: number, newTitle: string) {
+        
+        
+        const items = await this.items;
+        items.splice(index, 1, new Item(newTitle));
+        await this.saveListToDisk();
+
+     }
+
+
+    /**
+     * Remove um item da lista de item pelo índice
+     */
+    async removeItem(index: number) {
+        const items = await this.items
+        items.splice(index, 1)
+        await this.saveListToDisk()
   }
 
-  async updateItem(index: number, novoTexto: string) {
-    const items = await this.items
-    if (index < 0 || index >= items.length){
-      throw 'índice inválido.'
+
+    /**
+     * Retorna uma cópia da lista de itens
+     */
+    async getItems() {
+        const items = await this.items
+        return Array.from(items)
     }
-    if (!novoTexto || !novoTexto.trim()){
-      throw 'Novo Texto não pode ser vazio.'
-    }
-    items[index].title = novoTexto
-    await this.saveListToDisk()
-  }
-  /**
-   * Retona uma cópia da lista de itens
-   */
-  async getItems() {
-    const items = await this.items
-    return Array.from(items)
-  }
+    
 }
+
 
 export default TodoList
 export { Item, TodoList }
